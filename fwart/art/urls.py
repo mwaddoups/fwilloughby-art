5f43a3b2c8f6f35d4d341fd95748db3b1c14b75e
Mark Waddoups <mwaddoups@gmail.com>
2020-09-25 17:27:48 +0100

    Removed django

--- fwart/art/urls.py
+++
@@ -1,8 +0,0 @@
-from django.urls import path
-
-from . import views
-
-urlpatterns = [
-    path('', views.IndexView.as_view(), name='index'),
-    path('artwork/<int:pk>/', views.ArtDetailView.as_view(), name='detail')
-]
